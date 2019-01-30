dae4139a921307f04c13a9183ab7b15f422e13c6
SomiDeev <sowmyadvn.92@gmail.com>
2019-01-29 21:49:58 -0800

    O(max(M,N))S:O(max(M,N))T Beats 99% solutions 35M

--- Leetcode/AddNumbers_2.java
+++ Leetcode/AddNumbers_2.java
@@ -8,3 +8,61 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+/*
+Approach: Add two numbers with digits stored as node values in linked list. As LSD is head of linked list, it is easy to sum up and check if there is a carry. Make sure to check that both of them are null, because one list may have more digits than the other. 
+*/
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        ListNode head = new ListNode(l1.val + l2.val), ptr = head;
+        ListNode temp1 = l1.next, temp2 = l2.next;
+        int reminder = head.val/10;
+        if(head.val > 9)
+            head.val = head.val - 10;
+        
+        while(temp1 != null || temp2 != null) {
+            int x = (temp1 != null)?temp1.val:0;
+            int y = (temp2 != null)?temp2.val:0;
+            ListNode temp = new ListNode(x+y+reminder);
+            if(temp.val > 9) {
+                temp.val = temp.val % 10;
+                reminder = 1;
+            }
+            else
+                reminder = 0;
+            ptr.next = temp;
+            ptr = temp;
+            
+            if (temp1 != null) 
+                temp1 = temp1.next;
+            if (temp2 != null) 
+                temp2 = temp2.next;
+        }
+        if(reminder == 1) {
+            ListNode temp = new ListNode(1);
+            ptr.next = temp;
+        }
+        return head;
+    }
+}
