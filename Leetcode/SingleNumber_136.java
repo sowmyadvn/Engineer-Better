a41d1111444e783c955c5a3d841192f771e0b43e
SomiDeev <sowmyadvn.92@gmail.com>
2019-02-20 19:54:48 -0800

    O(N)T:O(1)S Beats 100% solutions

--- Leetcode/SingleNumber_136.java
+++ Leetcode/SingleNumber_136.java
@@ -1,3 +1,19 @@
+/*
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+*/
 class Solution {
     public int singleNumber(int[] nums) {
         int result = nums[0];
@@ -7,4 +23,7 @@ class Solution {
         return result;
     }
 }
-//O(N) time complexity
+//O(N) time complexity 
+/*
+Approach: XORing elements in array results in single element being separated
+*/
