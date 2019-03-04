8504ea7b704bab543a55605adee4370bf329b23c
SomiDeev <sowmyadvn.92@gmail.com>
2019-03-03 19:18:40 -0800

    O(log N)S:O(N)T Beats 100% solutions

--- Leetcode/MaxDepth_104.java
+++ Leetcode/MaxDepth_104.java
@@ -28,6 +28,23 @@ return its depth = 3.
  * }
 
  O(N)T and O(log N)S where N is number of nodes
+
+ Algorithm:
+
+ maxDepth()
+1. If tree is empty then return 0
+2. Else
+     (a) Get the max depth of left subtree recursively  i.e., 
+          call maxDepth( tree->left-subtree)
+     (a) Get the max depth of right subtree recursively  i.e., 
+          call maxDepth( tree->right-subtree)
+     (c) Get the max of max depths of left and right 
+          subtrees and add 1 to it for the current node.
+         max_depth = max(max dept of left subtree,  
+                             max depth of right subtree) 
+                             + 1
+     (d) Return max_depth
+     
  */
 class Solution {
     public int maxDepth(TreeNode root) {
