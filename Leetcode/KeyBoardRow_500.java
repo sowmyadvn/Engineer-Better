6817672e1fe10cae25b04fe98711910ec2fdbe40
SomiDeev <sowmyadvn.92@gmail.com>
2019-03-28 14:03:04 -0700

    O(N)S:O(NS)T Beats 99% solutions

--- Leetcode/KeyBoardRow_500.java
+++ Leetcode/KeyBoardRow_500.java
@@ -6,22 +6,27 @@ Example:
 Input: ["Hello", "Alaska", "Dad", "Peace"]
 Output: ["Alaska", "Dad"]
 */
+```
 class Solution {
     public String[] findWords(String[] words) {
         String[] rows = new String[3];
+        //Three rows of keyboard characters stored in array of strings
         rows[0] = "qwertyuiopQWERTYUIOP";
         rows[1] = "asdfghjklASDFGHJKL";
         rows[2] = "zxcvbnmZXCVBNM";
         List<String> strList = new ArrayList<String>();
         for(String s: words) {
             int j = 0, row = 0;
-            boolean InRow = false;
-            while(!InRow) {
+            boolean rowsChecked = false;
+            //rowsChecked = true if all rows are checked and string is evaluated
+            while(!rowsChecked) {
                 int i = 0;
                 for(i = 0; i < s.length() && j < rows.length;) {
                     String c = ""+s.charAt(i);
+                    //If char in row, check next char
                     if(rows[j].contains(c))
                         i++;
+                    //If char not in row, increment row and check from first in next Row
                     else {
                         j++;
                         i = 0;
@@ -31,9 +36,10 @@ class Solution {
                 if(i == s.length()) {
                     strList.add(s);
                 }
-                InRow = true;
+                rowsChecked = true;
             }
         }
         return strList.toArray(new String[strList.size()]);
     }
 }
+```
