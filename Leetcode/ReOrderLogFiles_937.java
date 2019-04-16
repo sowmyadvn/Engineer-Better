e744f32662ab0ae3a23bc412c37946ce8ca98d9f
SomiDeev <sowmyadvn.92@gmail.com>
2019-04-16 11:03:14 -0700

    Lots of space O(NlogN)T:O(N)S Beats 90%

--- Leetcode/ReOrderLogFiles_937.java
+++ Leetcode/ReOrderLogFiles_937.java
@@ -74,3 +74,32 @@ class Solution {
         return result;
      }
 }
+/*
+Instead of sorting in the default order, we'll sort in a custom order we specify.
+
+The rules are:
+
+Letter-logs come before digit-logs;
+Letter-logs are sorted alphanumerically, by content then identifier;
+Digit-logs remain in the same order.
+It is straightforward to translate these ideas into code.
+
+*/
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (log1, log2) -> {
+            String[] split1 = log1.split(" ", 2);
+            String[] split2 = log2.split(" ", 2);
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+            if (!isDigit1 && !isDigit2) {
+                int cmp = split1[1].compareTo(split2[1]);
+                if (cmp != 0) return cmp;
+                return split1[0].compareTo(split2[0]);
+            }
+            return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+        });
+        return logs;
+    }
+}
