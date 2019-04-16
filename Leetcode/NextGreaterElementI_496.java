85098554e6b1a6d259177827a413addc4463dad2
SomiDeev <sowmyadvn.92@gmail.com>
2019-04-16 15:53:06 -0700

    O(N*M)T:O(N)S Beats 12%

--- Leetcode/NextGreaterElementI_496.java
+++ Leetcode/NextGreaterElementI_496.java
@@ -45,3 +45,20 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int num: nums2) {
+            while(!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(),num);
+            } 
+            stack.push(num);
+        }
+        for(int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.getOrDefault(nums1[i],-1);
+        }
+        return nums1;
+    }
+}
