087efa76be65126ec2f18efa67a16aeae26bdcf4
SomiDeev <sowmyadvn.92@gmail.com>
2019-04-18 10:34:37 -0700

    O(N)S:O(N)T Beats 90%

---
+++ Leetcode/ReverseOnlyLetters_917.java
@@ -0,0 +1,44 @@
+/*
+Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+
+ 
+
+Example 1:
+
+Input: "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+ 
+
+Note:
+
+S.length <= 100
+33 <= S[i].ASCIIcode <= 122 
+S doesn't contain \ or "
+*/
+
+class Solution {
+    public String reverseOnlyLetters(String S) {
+        StringBuilder ans = new StringBuilder();
+        //Two pointers one for special characters and one for letters
+        int j = S.length() - 1;
+        for (int i = 0; i < S.length(); ++i) {
+            if (Character.isLetter(S.charAt(i))) {
+                while (!Character.isLetter(S.charAt(j)))
+                    j--;
+                ans.append(S.charAt(j--));
+            } else {
+                ans.append(S.charAt(i));
+            }
+        }
+
+        return ans.toString();
+    }
+}
