d989eb0f3e37f384cc32359a7572d4ca498a984d
SomiDeev <sowmyadvn.92@gmail.com>
2019-10-04 17:32:03 -0700

    O(N)S:O(N)T Beats 45%

---
+++ Leetcode/LargestUniqueNumber_1133.java
@@ -0,0 +1,48 @@
+/*
+Given an array of integers A, return the largest integer that only occurs once.
+
+If no integer occurs once, return -1.
+
+ 
+
+Example 1:
+
+Input: [5,7,3,9,4,9,8,3,1]
+Output: 8
+Explanation: 
+The maximum integer in the array is 9 but it is repeated. The number 8 occurs only once, so it's the answer.
+Example 2:
+
+Input: [9,9,8,8]
+Output: -1
+Explanation: 
+There is no number that occurs only once.
+ 
+
+Note:
+
+1 <= A.length <= 2000
+0 <= A[i] <= 1000
+*/
+class Solution {
+    public int largestUniqueNumber(int[] A) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i: A) {
+            if(map.containsKey(i)) {
+                int val = map.get(i);
+                map.put(i,++val);
+            }
+            else 
+                map.put(i,1);
+        }
+        int large = Integer.MIN_VALUE;
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()) {
+            if(entry.getValue() == 1 && entry.getKey() > large)
+                large = entry.getKey();   
+        }
+        if(large < -1)
+            return -1;
+        
+        return large;
+    }
+}
