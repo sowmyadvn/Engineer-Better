e1bfe03efca7e0d866a97453636202914fc31e7f
SomiDeev <sowmyadvn.92@gmail.com>
2019-10-07 17:53:41 -0700

    O(MN)T:O(S)S Beats 7%

---
+++ Leetcode/IntersectionThreeArrays_1213.java
@@ -0,0 +1,38 @@
+/*
+Given three integer arrays arr1, arr2 and arr3 sorted in strictly increasing order, return a sorted array of only the integers that appeared in all three arrays.
+
+ 
+
+Example 1:
+
+Input: arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
+Output: [1,5]
+Explanation: Only 1 and 5 appeared in the three arrays.
+ 
+
+Constraints:
+
+1 <= arr1.length, arr2.length, arr3.length <= 1000
+1 <= arr1[i], arr2[i], arr3[i] <= 2000
+*/
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> arr = new ArrayList<Integer>();
+        for(int i = 0; i < arr1.length; i++) {
+            boolean two = false, three = false;
+            for(int j = 0; j < arr2.length && arr2[j] <= arr1[i]; j++) {
+                if(arr1[i] == arr2[j]) {
+                    two = true;
+                }
+            }
+            for(int k = 0; k < arr3.length && arr3[k] <= arr1[i]; k++) {
+                if(arr1[i] == arr3[k]) {
+                    three = true;
+                }
+            }
+            if(two && three)
+                arr.add(arr1[i]);
+        }
+        return arr;
+    }
+}
