5636dd34dccc1e84cf0f70126c871b06ddc1ca9c
SomiDeev <sowmyadvn.92@gmail.com>
2019-10-07 17:59:14 -0700

    O(M)T:O(S)S Beats 100%%

--- Leetcode/IntersectionThreeArrays_1213.java
+++ Leetcode/IntersectionThreeArrays_1213.java
@@ -36,3 +36,27 @@ class Solution {
         return arr;
     }
 }
+
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> result = new ArrayList<Integer>();
+        int i = 0, j = 0, k = 0;
+        while(i < arr1.length && j < arr2.length && k < arr3.length) {
+            if(arr1[i] == arr2[j] && arr1[i] == arr3[k]) {
+                result.add(arr1[i]);
+                i++;
+                j++;
+                k++;
+            }
+            else if(arr1[i] < arr2[j]) {
+                i++;
+            }
+            else if(arr2[j] < arr3[k]) {
+                j++;
+            }
+            else 
+                k++;
+        }
+        return result;
+    }
+}
