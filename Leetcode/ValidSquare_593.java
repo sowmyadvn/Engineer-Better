ce2282746e8169d6984f5bf1957b780aefdaf22e
SomiDeev <sowmyadvn.92@gmail.com>
2019-10-09 11:39:16 -0700

    O(1)S:O(1)T Beats 81%

--- Leetcode/ValidSquare_593.java
+++ Leetcode/ValidSquare_593.java
@@ -14,7 +14,7 @@ Note:
 All the input integers are in the range [-10000, 10000].
 A valid square has four equal sides with positive length and four equal angles (90-degree angles).
 Input points have no order.
- 
+
 */
 class Solution {
     public double dist(int[] p1, int[] p2) {
@@ -25,5 +25,17 @@ class Solution {
         Arrays.sort(p, (l1, l2) -> l1[0] == l2[0] ? l1[1] - l2[1] : l1[0] - l2[0]);
         
         return dist(p[0],p[1]) != 0 && dist(p[0],p[1]) == dist(p[1],p[3]) && dist(p[1],p[3]) == dist(p[3],p[2]) && dist(p[3],p[2]) == dist(p[2],p[0]) && dist(p[0],p[3]) == dist(p[1],p[2]);
+    }
+}
+//better performance as doesn't require sorting
+public class Solution {
+    public double dist(int[] p1, int[] p2) {
+        return (p2[1] - p1[1]) * (p2[1] - p1[1]) + (p2[0] - p1[0]) * (p2[0] - p1[0]);
+    }
+    public boolean check(int[] p1, int[] p2, int[] p3, int[] p4) {
+        return dist(p1,p2) > 0 && dist(p1, p2) == dist(p2, p3) && dist(p2, p3) == dist(p3, p4) && dist(p3, p4) == dist(p4, p1) && dist(p1, p3) == dist(p2, p4);
+    }
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        return check(p1, p2, p3, p4) || check(p1, p3, p2, p4) || check(p1, p2, p4, p3);
     }
 }
