b4833d66bd1fb2313c2121bcb14502c1965c3a10
SomiDeev <sowmyadvn.92@gmail.com>
2019-10-30 17:09:05 -0700

    O(N)T

--- Leetcode/PascalTriangle_118.java
+++ Leetcode/PascalTriangle_118.java
@@ -35,3 +35,18 @@ Output:
  [1,4,6,4,1]
 ]
 */
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> pascal = new ArrayList<List<Integer>>();
+        List<Integer> curr = new ArrayList<>();
+        for(int i = 0; i < numRows;i++) {
+            curr.add(0,1);
+            for(int j = 1; j < curr.size()-1;j++) {
+                curr.set(j,curr.get(j)+curr.get(j+1));
+            }
+            pascal.add(new ArrayList<Integer>(curr));
+        }
+        return pascal;
+    }
+}
