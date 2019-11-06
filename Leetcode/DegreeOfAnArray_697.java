3ce805cec350bc49b9de581a1714f791fc0d3d5c
SomiDeev <sowmyadvn.92@gmail.com>
2019-11-05 19:29:25 -0800

    O(N)T

--- Leetcode/DegreeOfAnArray_697.java
+++ Leetcode/DegreeOfAnArray_697.java
@@ -1,3 +1,5 @@
+/* Another smart approach is to use list to store degree,first and last elements */
+
 class Solution {
     public int findShortestSubArray(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
@@ -45,3 +47,22 @@ Note:
 nums.length will be between 1 and 50,000.
 nums[i] will be an integer between 0 and 49,999.
 */
+
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer,Integer> count = new HashMap<>(), first = new HashMap<>();
+        int degree = 0, res = 0;
+        for(int i = 0; i < nums.length; i++) {
+            first.putIfAbsent(nums[i],i);
+            count.put(nums[i],count.getOrDefault(nums[i],0)+1);
+            if(count.get(nums[i]) > degree) {
+                degree = count.get(nums[i]);
+                res = i-first.get(nums[i])+1;
+            }
+            else if(count.get(nums[i]) == degree) {
+                res = Math.min(res, i - first.get(nums[i])+1);
+            }
+        }
+        return res;
+    }
+}
