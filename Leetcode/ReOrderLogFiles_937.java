f2c0621196d0102ca86e8b5250f5e53eb79787d9
SomiDeev <sowmyadvn.92@gmail.com>
2019-11-15 19:43:07 -0800

    O(N)S:O(N)T Beats 100%

--- Leetcode/ReOrderLogFiles_937.java
+++ Leetcode/ReOrderLogFiles_937.java
@@ -25,6 +25,38 @@ Note:
 3 <= logs[i].length <= 100
 logs[i] is guaranteed to have an identifier, and a word after the identifier.
 */
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Comparator<String> myComp = new Comparator<String>() {
+            @Override
+            public int compare(String s1,String s2) {
+                int s1k = s1.indexOf(' ');
+                int s2k = s2.indexOf(' ');
+                char s1c = s1.charAt(s1k+1);
+                char s2c = s2.charAt(s2k+1);
+
+                if(s1c <= '9') {
+                    if(s2c <= '9') 
+                        return 0;
+                    else 
+                        return 1;
+                }
+
+                if(s2c <= '9') return -1;
+
+                int preCompute = s1.substring(s1k+1).compareTo(s2.substring(s2k+1));
+                if (preCompute == 0) return s1.substring(0,s1k).compareTo(s2.substring(0,s2k));
+                    return preCompute;
+             }
+        };
+        
+    Arrays.sort(logs, myComp);
+    return logs;
+    }
+}
+
+
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
         //Store alphalog+indices of occurrences
@@ -73,33 +105,4 @@ class Solution {
         }
         return result;
      }
-}
-/*
-Instead of sorting in the default order, we'll sort in a custom order we specify.
-
-The rules are:
-
-Letter-logs come before digit-logs;
-Letter-logs are sorted alphanumerically, by content then identifier;
-Digit-logs remain in the same order.
-It is straightforward to translate these ideas into code.
-
-*/
-
-class Solution {
-    public String[] reorderLogFiles(String[] logs) {
-        Arrays.sort(logs, (log1, log2) -> {
-            String[] split1 = log1.split(" ", 2);
-            String[] split2 = log2.split(" ", 2);
-            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
-            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
-            if (!isDigit1 && !isDigit2) {
-                int cmp = split1[1].compareTo(split2[1]);
-                if (cmp != 0) return cmp;
-                return split1[0].compareTo(split2[0]);
-            }
-            return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
-        });
-        return logs;
-    }
 }
