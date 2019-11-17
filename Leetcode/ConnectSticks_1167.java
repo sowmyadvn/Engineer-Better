4c8757721e19b7472fef63c2c504e0e23566b002
SomiDeev <sowmyadvn.92@gmail.com>
2019-11-17 00:07:59 -0800

    O(N)T Beats 67%

---
+++ Leetcode/ConnectSticks_1167.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int connectSticks(int[] sticks) {
+        if(sticks == null || sticks.length <= 1) return 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();    
+        for(int i: sticks)
+            pq.offer(i);
+        int cost = 0;
+        while(pq.size() > 1) {
+            int tSum = pq.poll()+pq.poll();
+            cost += tSum;
+            pq.offer(tSum);
+        }
+        return cost;
+    }
+}
+/*
+You have some sticks with positive integer lengths.
+
+You can connect any two sticks of lengths X and Y into one stick by paying a cost of X + Y.  You perform this action until there is one stick remaining.
+
+Return the minimum cost of connecting all the given sticks into one stick in this way.
+
+ 
+
+Example 1:
+
+Input: sticks = [2,4,3]
+Output: 14
+Example 2:
+
+Input: sticks = [1,8,3,5]
+Output: 30
+ 
+
+Constraints:
+
+1 <= sticks.length <= 10^4
+1 <= sticks[i] <= 10^4
+*/
