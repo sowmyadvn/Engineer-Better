fec12f6139661a190149eeeff1d1c905eb94c131
SomiDeev <sowmyadvn.92@gmail.com>
2019-11-18 17:00:52 -0800

    O(N)T:O(N)S

--- Leetcode/VerifyPreOrder_255.java
+++ Leetcode/VerifyPreOrder_255.java
@@ -12,6 +12,19 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    int i = 1;
+    public boolean verifyPreorder(int[] A) {
+        return (A.length == 0 || (verifyPreorder(A,Integer.MIN_VALUE,A[0]) && verifyPreorder(A,A[0],Integer.MAX_VALUE))); 
+    }
+    
+    private boolean verifyPreorder(int[] A, int left, int right) {
+        if(i == A.length || A[i] > right) return true;
+        int mid = A[i++];
+        return mid > left && verifyPreorder(A,left,mid) && verifyPreorder(A,mid,right);
+    }
+}
 /*
 Given an array of numbers, verify whether it is the correct preorder traversal sequence of a binary search tree.
 
