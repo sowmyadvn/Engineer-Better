258c0f69aac43687213033fcdc0860ba20edb468
SomiDeev <sowmyadvn.92@gmail.com>
2020-02-17 14:10:25 -0800

    Product except itself

--- Leetcode/ProductOfArrayExceptItself_238.java
+++ Leetcode/ProductOfArrayExceptItself_238.java
@@ -13,6 +13,34 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        int prod = 1;
+        left[0] = 1;
+        // left product
+        for( int i = 1; i < nums.length; i++) {
+            left[i] = nums[i-1]*left[i-1];
+        }
+        
+        right[nums.length - 1] = 1;
+        prod = 1;
+
+        //right product
+        for( int i = nums.length-2; i >= 0; i--) {
+            right[i] = nums[i+1]*right[i+1];
+        }
+
+        //updating array with the result
+        for(int i = 0; i < nums.length; i++) {
+            right[i] = right[i] * left[i];
+        }
+        
+        return right;
+    }
+}
 /*
 Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
 
