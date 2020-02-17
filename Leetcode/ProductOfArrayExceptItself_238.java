641160576ae5893e2996d981da2812cc8fa0c052
SomiDeev <sowmyadvn.92@gmail.com>
2020-02-17 14:11:51 -0800

    Product except itself

--- Leetcode/ProductOfArrayExceptItself_238.java
+++ Leetcode/ProductOfArrayExceptItself_238.java
@@ -1,18 +1,17 @@
-class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int[] result = new int[nums.length];
-        result[0] = 1;
-        for(int i = 1; i < nums.length; i++) {
-            result[i] = result[i-1]*nums[i-1];
-        }
-        int right = 1;
-        for(int i = nums.length-1; i >= 0; i--) {
-            result[i] *= right;
-            right *= nums[i];
-        }
-        return result;
-    }
-}
+// Time Complexity : O(N) for both
+// Space Complexity : O(N) for approach-1 and O(1) for approach-2
+// Did this code successfully run on Leetcode : yes
+
+
+/*
+Approach 
+========
+1. First update the result array with product of every element's left elements.
+2. Now, iterate from right to left, keeping track of right product so far using a "prod" variable. 
+3. Multiply value in result array with the prod variable and keep updating prod variable to get product of array elements except itself.
+*/
+
+// ==================== Approach - 1 using O(N) space ===============================
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
@@ -41,6 +40,32 @@ class Solution {
         return right;
     }
 }
+
+// ==================== Approach - 2 using constant space ===============================
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = 1;
+
+        // calculate left sub-product
+        for(int i = 1; i < nums.length; i++) {
+            result[i] = result[i-1]*nums[i-1];
+        }
+
+        // Use right variable to keep track of right product so far and update result array with right subproduct as well
+        // result would be result of right and left subproducts after this step
+        int right = 1;
+        for(int i = nums.length-1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+}
+
+
 /*
 Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
 
