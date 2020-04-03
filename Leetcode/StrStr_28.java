388ddb0f95c17e4f429391026142598615d37095
SomiDeev <sowmyadvn.92@gmail.com>
2020-04-02 19:01:40 -0700

    some more

--- Leetcode/StrStr_28.java
+++ Leetcode/StrStr_28.java
@@ -18,8 +18,29 @@ For the purpose of this problem, we will return 0 when needle is an empty string
 
 */
 
+// ============= Using built-in indexof ========================
+
 class Solution {
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
+    }
+}
+
+// ================ Without using indexOf ========================
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle == null || needle.length() == 0) return 0;
+        if(haystack == null || haystack.length() == 0 || haystack.length() < needle.length()) return -1;
+        
+        int nsize = needle.length();
+        int start = 0, end = nsize;
+        while(end <= haystack.length()) {
+            if(haystack.substring(start, end).equals(needle))
+                return start;
+            start++;
+            end++;
+        }
+        return -1;
     }
 }
